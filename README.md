9ecf5d69dcedd8cdb3dcfa3ab3dc8b83246b460f
Giannis <bone.jp@gmail.com>
2013-10-25 12:49:05 +0200

    second release

--- README.md
+++
@@ -1,4 +0,0 @@
-cubiccloud.com
-==============
-
-me
--- js/jsb.js
+++ js/jsb.js
@@ -1,1 +1,272 @@
-var accordNum=0;var accor={checkUrl:function(){var A=false;$$("a.button").each(function(C,B){if(window.location.hash.test(C.hash)){accordNum=B;A=true}});return A;if(!checkHash()){accordNum=0}},mainAccordion:function(){$$("div.warper").each(function(A){new Accordion(A.getElements("a.button"),A.getElements("div.container"),{fixedHeight:550,fixedWidth:800,opacity:false,display:accordNum})})},init:function(){this.checkUrl();this.mainAccordion()}};var accor2={portfolioAccordion:function(){$$("div.portfoliocontent").each(function(C){var D=new Fx.Styles(C,{duration:500});var B=0;var A=0;new Accordion(C.getElements("a.postbutton"),C.getElements("div.port"),{alwaysHide:true,start:"all-closed",onActive:function(E){E.setStyle("color","#ffffff");B++;A-=5;if(B===1){D.start({"padding-top":[350,10],"padding-left":[480,330]})}},onBackground:function(E){E.setStyle("color","#CCCCCC");A++;if(A===6){D.start({"padding-top":[10,350],"padding-left":[330,480]});A=0;B=0}}})})},init:function(){this.portfolioAccordion()}};var tip={makeTip:function(){new Tips($$(".tips"),{initialize:function(){this.fx=new Fx.Style(this.toolTip,"opacity",{duration:400,wait:false}).set(0)},onShow:function(A){this.fx.start(1)},onHide:function(A){this.fx.start(0)}})},init:function(){this.makeTip()}};window.addEvent("domready",function(){accor.init();accor2.init();tip.init();$("contactform").addEvent("submit",function(B){new Event(B).stop();var A=$("log_res").empty().addClass("ajax-loading");this.send({update:A,onComplete:function(){A.removeClass("ajax-loading")}})})});
+/*!
+ * cubicloud.com
+ * Version: 1.5
+ * Updated: 02-09-2010
+ * http://cubiccloud.com/
+ *
+ * Copyright (c) 2009 paramana.com
+ *
+ */
+
+//Activate FancyBox
+var gallery = {
+    makeGallery: function() {
+        $(".gallery a").fancybox({
+            'overlayShow' : true,
+            'padding' : 10,
+            'frameWidth' : 660,
+            'frameHeight': 505,
+            'hideOnContentClick' : false
+        });
+        $(".videolink").fancybox({
+            'overlayShow' : true,
+            'padding' : 10,
+            'frameWidth' : 480,
+            'frameHeight': 360,
+            'hideOnContentClick' : false
+        });
+    },
+    init: function(){
+        this.makeGallery();
+    }
+};
+var animations = {
+    accordionMain: function() {
+        $('#footer a').click(function(){
+            if(!$(this).attr('target')) {
+                var _thisId = $(this).attr('href');
+                $('.container').each(function (){
+                    if ($(this).children(':eq(0)').attr('id') != _thisId) {
+                        $(this).slideUp();
+                    }
+                    else
+                        $(this).slideDown();
+                });
+                window.location.hash = _thisId;
+                return false;
+            }
+        });
+    },
+    accordionPort: function() {
+        $('.port').hide();
+        $('.postbutton').click(function(){
+            var thisId = $(this).attr('href');
+            if(!$(this).data('open_port') || $(this).data('open_port') == false) {
+                $('.postbutton').data('open_port', false);
+                $(this).data('open_port', true);
+                $('.postbutton').css('color', '#CCCCCC');
+                
+                $(this).css('color', '#ffffff');
+
+                $('#portfoliocontent').animate({
+                    paddingTop: 10,
+                    paddingLeft: 330
+                });
+                $('.port').slideUp();
+                $('#' + thisId).slideDown();
+            }
+            else {
+                $(this).css('color', '#CCCCCC');
+                $('.postbutton').data('open_port', false);
+                $('#portfoliocontent').animate({
+                    paddingTop: 350,
+                    paddingLeft: 480
+                });
+                $('.port').slideUp();
+            }
+            
+            //alert($('#' + $(this).attr('href')));
+            return false;
+        });
+    },
+    redirectUrl: function(){
+        var hash = window.location.hash.substr(1);
+        var found = false;
+        if (hash != '') {
+            $('#footer a').each(function(){
+                var href = $(this).attr('href');
+                if(hash == href){
+                    $('#' + href).parent().show();
+                    found = true;
+                }
+            });
+            if(!found)
+                $('#about').parent().show();
+            
+        }
+        else
+            $('#about').parent().show();
+    },
+    init: function(){
+        //this.contentFader();
+        this.redirectUrl();
+        this.accordionMain();
+        this.accordionPort();
+    }
+};
+$(function extrnalLinks () {
+    $("a.externalLink").attr("target", "_blank");
+});
+
+var webStart = {
+    siteLoader: function() {
+        $(window).load(function () {
+            //$("#siteLoading,#siteLoadOverlay_Frame").fadeOut("fast").remove();
+            $("#siteLoading").fadeOut('normal', function (){
+                $(this).remove();
+            });
+        });
+        $('.container').hide();
+    },
+    init: function(){
+        this.siteLoader();
+    }
+};
+$(function contactForma () {
+    $('.error').hide();
+    $("#submit").click(function() {
+        // validate and process form
+        // first hide any error messages
+        $('.error').hide();
+
+        var name = $("input#name").val();
+        if (name == "" || name == "your name..") {
+            $("#nameError").fadeIn('fast');
+            $("input#name").focus();
+            return false;
+        }
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        var email = $("input#email").val();
+        if (email == "" || email == "your e-mail goes here..") {
+            $("#emailError").fadeIn('fast');
+            $("input#email").focus();
+            return false;
+        }
+        else if (!emailReg.test(email)) {
+            $("#emailWrong").fadeIn('fast');
+            $("input#email").focus();
+            return false;
+        }
+        var message = $("textarea#message").val();
+        if (message == "") {
+            $("#messageError").fadeIn('fast');
+            $("textarea#message").focus();
+            return false;
+        }
+
+        var dataString = 'name='+ name + '&email=' + email + '&message=' + message;
+
+        $.ajax({
+            type: "POST",
+            url: "bin/process.php",
+            data: dataString,
+            success: function() {
+                $('#forma').html("<div id='feedback'><h3>Contact Form Submitted!</h3><p>I will be in touch soon.</p><img id='checkmark' src='images/mailbox.png' alt='' /></div>");
+            }
+        });
+        return false;
+    });
+});
+
+jQuery.fn.clearFields = function(focusClass) {
+    this.each(function() {
+        $(this).focus(function() {
+            // clear value if current value is the default
+            if($(this).val() == this.defaultValue) {
+                $(this).val("");
+            }
+
+            // if focusClass is set, add the class
+            if(focusClass) {
+                $(this).addClass(focusClass);
+            }
+        }).blur(function() {
+            // restore to the default value if current value is empty
+            if($(this).val() == "") {
+                $(this).val(this.defaultValue);
+            }
+
+            // if focusClass is set, remove class
+            if(focusClass) {
+                $(this).removeClass(focusClass);
+            }
+        });
+    });
+}
+$(document).ready( function(){
+    $("#name, #email, #message").clearFields("activeField");
+    $("#message").clearForm();
+    $("#name").val("your name..");
+    $("#email").val("your e-mail goes here..");
+    $("input#name").keypress(function (e){
+        $("#nameError").fadeOut('slow');
+    });
+    $("input#email").keypress(function (e){
+        $("#emailError").fadeOut('slow');
+        $("#emailWrong").fadeOut('slow');
+    });
+    $("textarea#message").keypress(function (e){
+        $("#messageError").fadeOut('slow');
+    });
+    webStart.init();
+    animations.init();
+    tooltip();
+    gallery.init();
+});
+
+this.tooltip = function(){
+    /* CONFIG */
+    xOffset = -10;
+    yOffset = 20;
+    // these 2 variable determine popup's distance from the cursor
+    // you might want to adjust to get the right result
+    /* END CONFIG */
+    $("a.tooltip").hover(function(e){
+        this.t = this.title;
+        this.title = "";
+        var tipContent = this.t.split('::');
+        $("body").append('<div id="tooltip">\
+                            <div>\
+                                <div class="tool-title">\
+                                    <span>' + tipContent[0] + '</span>\
+                                </div>\
+                                <div class="tool-text">\
+                                    <span>'+
+                                        tipContent[1]
+                                    +'</span>\
+                                </div>\
+                            </div>\
+                        </div>');
+        $("#tooltip")
+        .css("top",(e.pageY - xOffset) + "px")
+        .css("left",(e.pageX + yOffset) + "px")
+        .fadeIn("fast");
+    },
+    function(){
+        this.title = this.t;
+        $("#tooltip").remove();
+    });
+    $("a.tooltip").mousemove(function(e){
+        $("#tooltip")
+        .css("top",(e.pageY - xOffset) + "px")
+        .css("left",(e.pageX + yOffset) + "px");
+    });
+    $("a.tooltip").click(function(e){
+        return false;
+    });
+};
+
+$.fn.clearForm = function() {
+    return this.each(function() {
+        var type = this.type, tag = this.tagName.toLowerCase();
+        if (tag == 'form')
+            return $(':input',this).clearForm();
+        if (type == 'text' || type == 'password' || tag == 'textarea')
+            this.value = '';
+        else if (type == 'checkbox' || type == 'radio')
+            this.checked = false;
+        else if (tag == 'select')
+            this.selectedIndex = -1;
+    });
+};
+
