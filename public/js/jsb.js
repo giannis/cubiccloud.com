1c48b55be00dbf2b1fd388d662aba344bb685843
Giannis <bone.jp@gmail.com>
2013-10-27 15:39:08 +0100

    updates

---
+++ public/js/jsb.js
@@ -0,0 +1,106 @@
+/*!
+ * cubiccloud.com
+ * Version: 2.0
+ * Started: 26-10-2013
+ * Updated: 26-10-2013
+ * Url    : http://www.cubiccloud.com
+ * Author : giannis (giannis AT paramana DOT com)
+ *
+ * Copyright (c) 2013 paramana.com
+ *
+ */
+
+/*
+ * Self calling function for our app
+ *
+ */
+(function(window){
+    var App = {};
+    
+    App.gmap = {
+        map    : {},
+        marker : {},
+        coordinates : [52.354456, 4.91887],//mpraxami 37.941932,23.730001
+        resize: function(){
+            $('#map-canvas').css('height', $(window).height() * 0.5);
+            this.map.setCenter(new google.maps.LatLng(this.coordinates[0], this.coordinates[1]));
+        },
+        init: function(){
+            var mapStyle = [
+                { featureType: "water", stylers: [ { color: "#9fc0dd" } ] },
+                { elementType: "labels.text.stroke", stylers: [ { color: "#feffff" } ] },
+                { featureType: "road", elementType: "geometry.fill", stylers: [ { color: "#ffffff" } ] },
+                { elementType: "geometry.stroke", stylers: [ { visibility: "off" } ] },
+                { featureType: "poi.attraction", elementType: "geometry", stylers: [ { visibility: "off" } ] },
+                { featureType: "poi.business", stylers: [ { visibility: "off" } ] },
+                { featureType: "poi.government", stylers: [ { visibility: "off" } ] },
+                { featureType: "poi.medical", stylers: [ { visibility: "off" } ] },
+                { featureType: "poi.place_of_worship", stylers: [ { visibility: "off" } ] },
+                { featureType: "poi.school", stylers: [ { visibility: "off" } ] },
+                { featureType: "poi.sports_complex", stylers: [ { visibility: "off" } ] },
+                { featureType: "transit.line", elementType: "geometry", stylers: [ { color: "#cccccc" } ] },
+                { featureType: "transit.station", stylers: [ { visibility: "off" } ] },
+                { featureType: "administrative", stylers: [ { visibility: "off" } ] },
+                { featureType: "administrative.locality", stylers: [ { visibility: "on" } ] },
+                { featureType: "road", elementType: "labels.icon", stylers: [ { visibility: "off" } ] },
+                { featureType: "road.highway", elementType: "geometry", stylers: [ { color: "#FDFD90" } ] },
+                { featureType: "landscape.man_made", stylers: [ { color: "#cccccc" } ] }
+            ];
+
+            var myOptions = { 
+                center: new google.maps.LatLng(this.coordinates[0], this.coordinates[1]), 
+                panControl: false, 
+                mapTypeControl: false, 
+                streetViewControl: false, 
+                zoomControlOptions: { style: google.maps.ZoomControlStyle.SMALL }, 
+                styles : mapStyle, 
+                zoom: 13, 
+                scrollwheel: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            this.map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+            var markerImage = new google.maps.MarkerImage('images/cloud-marker-30x19.png',
+                null, 
+                new google.maps.Point(0,0),
+                new google.maps.Point(10,10)
+            );
+
+            this.marker = new google.maps.Marker({ 
+                position: new google.maps.LatLng(this.coordinates[0], this.coordinates[1]), 
+                map: this.map, 
+                icon: markerImage
+            });
+
+            $('#map-canvas').css('height', $(window).height() * 0.5);
+        }
+    };
+    
+    App.event = {
+        
+    };
+    
+    $(window)
+            .scroll(function() {
+                if ($(window).scrollTop() <= 20)
+                    $('.header').removeClass('scroll')
+                else
+                    $('.header').addClass('scroll')
+            })
+            .resize(function() {
+                App.gmap.resize();
+            });
+
+    $(document).ready(function(){
+//        $('html,body').animate({
+//            scrollTop: 0
+//        }, {
+//            duration: 'slow', easing: 'swing'
+//        });
+        App.gmap.init();
+    });
+
+    // Expose App to the global object
+    window.App = App;
+})(window);
